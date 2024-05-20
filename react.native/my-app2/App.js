c586ae5c57aaf60c772734dad65c943a9497c567
Emily <emily.rsilva@hotmail.com>
2024-05-20 14:02:08 -0300

    1°commit

---
+++ react.native/my-app2/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Text}>Emily 💗</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'lightblue',
+  },
+
+  Text:{
+    fontSize: '40rem',
+    fontWeight: 'bold',
+  }
+});
